b9c9cc07d6840b83d21bdf9fe8e364b7446334ed
Abi See <abigail.e.see@gmail.com>
2017-05-12 08:31:33 -0700

    fix small bug in outputids2words and clarify error messages

--- data.py
+++ data.py
@@ -193,12 +193,12 @@ def outputids2words(id_list, vocab, article_oovs):
     try:
       w = vocab.id2word(i) # might be [UNK]
     except ValueError as e: # w is OOV
-      assert FLAGS.pointer_gen, "Error: model produced a word ID that isn't in the vocabulary"
+      assert article_oovs is not None, "Error: model produced a word ID that isn't in the vocabulary. This should not happen in baseline (no pointer-generator) mode"
       article_oov_idx = i - vocab.size()
       try:
         w = article_oovs[article_oov_idx]
       except ValueError as e: # i doesn't correspond to an article oov
-        raise ValueError('Error: index %i corresponds to article OOV %i but this example only has %i article OOVs' % (i, article_oov_idx, len(article_oovs)))
+        raise ValueError('Error: model produced word ID %i which corresponds to article OOV %i but this example only has %i article OOVs' % (i, article_oov_idx, len(article_oovs)))
     words.append(w)
   return words
 
