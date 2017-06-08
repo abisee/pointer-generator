ff8649b231cc0704dc9c2ec1d56b8b9c34485b5a
pltrdy <pltrdy@gmail.com>
2017-06-08 11:35:04 +0200

    Use best model for decoding

--- util.py
+++ util.py
@@ -27,12 +27,25 @@ def get_config():
   config.gpu_options.allow_growth=True
   return config
 
-def load_ckpt(saver, sess):
+def load_ckpt(saver, sess, load_best=False):
   """Load checkpoint from the train directory and restore it to saver and sess, waiting 10 secs in the case of failure. Also returns checkpoint name."""
   while True:
     try:
+      ckpt_state = None
+      if load_best:
+        eval_dir = os.path.join(FLAGS.log_root, "eval")
+        tf.logging.info("Trying to load model from: %s" % eval_dir)
+        if os.path.exists(eval_dir):
+          try:
+            ckpt_state = tf.train.get_checkpoint_state(eval_dir, latest_filename="checkpoint_best")
+            tf.logging.info("Loaded best model %s" % ckpt_state.model_checkpoint_path)
+          except ValueError:
+            tf.logging.info("No best model (run eval)")
+      
       train_dir = os.path.join(FLAGS.log_root, "train")
-      ckpt_state = tf.train.get_checkpoint_state(train_dir)
+      if ckpt_state is None:
+        ckpt_state = tf.train.get_checkpoint_state(train_dir)
+
       tf.logging.info('Loading checkpoint %s', ckpt_state.model_checkpoint_path)
       saver.restore(sess, ckpt_state.model_checkpoint_path)
       return ckpt_state.model_checkpoint_path
