e9bfc3ce97526ed3264a987c78f19a74e8ffe921
Abi See <abigail.e.see@gmail.com>
2017-08-04 20:33:58 -0700

    Update batcher.py

--- batcher.py
+++ batcher.py
@@ -361,7 +361,7 @@ class Batcher(object):
         article_text = e.features.feature['article'].bytes_list.value[0] # the article text was saved under the key 'article' in the data files
         abstract_text = e.features.feature['abstract'].bytes_list.value[0] # the abstract text was saved under the key 'abstract' in the data files
       except ValueError:
-        tf.logging.error('Failed to get article or abstract from example: %s', text_format.MessageToString(e))
+        tf.logging.error('Failed to get article or abstract from example')
         continue
       if len(article_text)==0: # See https://github.com/abisee/pointer-generator/issues/1
         tf.logging.warning('Found an example with empty article text. Skipping it.')
